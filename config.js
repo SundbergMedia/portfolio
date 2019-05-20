40ae4fceb095e8434fccac50b1ee1f5083afefd7
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-05-20 22:42:39 +0200

    Initial config

---
+++ config.js
@@ -0,0 +1,39 @@
+const pkg = require('./package.json')
+const appName = 'app'
+const version = appName + pkg.version
+const baseSrc = 'src/'
+
+module.exports = {
+  name: appName,
+  version: version,
+  environment: 'DEVELOPMENT',
+  port: 6666,
+  browserSyncPort: 8285,
+  path: 'dist/',
+  bsFiles: 'dist/**/*.*',
+  bsConfig: {
+    server: {
+      baseDir: './dist/'
+    },
+    files: [
+      './dist/' + version + '.css',
+      './dist/' + version + '.js',
+      './dist/**/*.html'
+    ]
+  },
+  pugConfig: {
+    compileDebug: true,
+    pretty: true,
+    verbose: true
+  },
+  paths: {
+    js: baseSrc + 'js/**.js',
+    style: baseSrc + 'style/**.styl',
+    views: baseSrc + 'content/**.pug'
+  },
+  out: {
+    css: version + '.css',
+    js: version + '.js',
+    jsMin: version + '-min.js'
+  }
+}
